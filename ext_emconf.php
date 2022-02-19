83a86ec050e80def4e1ee65ab9215aa318f7688c
marco <kontakt@schmidt-webmedia.de>
2022-02-19 15:04:24 +0100

    #CHANGE Version and dependencies

--- ext_emconf.php
+++ ext_emconf.php
@@ -4,14 +4,13 @@ $EM_CONF[$_EXTKEY] = [
     'title' => 'Grid for Container',
     'description' => 'Add an easy to use grid element for Container. It\'s Framework independent. Prepared for Bootstrap.',
     'category' => 'plugin',
-    'version' => '0.9.0',
-    'state' => 'beta',
+    'version' => '1.0.0',
+    'state' => 'stable',
     'author' => 'Marco Schmidt',
     'author_email' => 'typo@schmidt-webmedia.de',
     'author_company' => 'Marco Schmidt - Webmedia',
     'constraints' => [
         'depends' => [
-            'php' => '',
             'typo3' => '10.4.0-11.5.99',
             'container' => ''
         ],
